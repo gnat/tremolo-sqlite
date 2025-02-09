72512f5e8f1f93b8403b653a958b3a1d86feafa3
Nathaniel Sabanski <sabanski.n@gmail.com>
2025-02-08 20:32:47 -0800

    Create server.py

---
+++ server.py
@@ -0,0 +1,20 @@
+import asyncio, time, sqlite3, json
+from tremolo import Tremolo
+
+con = sqlite3.connect("test.sqlite")
+con.cursor().execute(open("schema.sql").read())
+html = open("index.html").read()
+
+app = Tremolo()
+
+@app.route('/')
+async def hello_world(**server):
+	cur = con.cursor()
+	cur.execute("INSERT INTO clicks(time) VALUES (unixepoch())")
+	con.commit()
+	res = cur.execute("SELECT * FROM clicks")
+	out = html.replace('__DATA__', json.dumps(res.fetchall())).replace("],","],\n")
+	return out
+
+if __name__ == '__main__':
+	app.run('0.0.0.0', 8080, worker_num=1, reload=True, debug=True)
